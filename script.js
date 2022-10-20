a87fe4b12e22416def55d31c05b966df2b658b90
TildaL <112630571+TildaL@users.noreply.github.com>
2022-10-20 13:05:56 +0200

    Update script.js
    updates

--- script.js
+++ script.js
@@ -26,3 +26,45 @@ function getMonth (month) {
 getMonth('2018-04-05');
 
 console.log( getMonth('2018-04-05') );
+
+//Övning 3 
+
+function getSum (numberOne, numberTwo, operator) {
+    let result = 0;
+    if (operator === '+') {
+        result = numberOne + numberTwo;    
+    }
+    else if (operator === '-') {
+        result = numberOne - numberTwo;  
+    }
+    else if (operator === '*') {
+        result = numberOne * numberTwo;  
+    }
+    else if (operator === '/') {
+        result = numberOne / numberTwo;  
+    }
+    return result;
+}
+
+console.log(getSum(9,3,'/'));
+console.log(getSum(15,3,'*'));
+
+//Övning 4
+
+for (let i=0; i<3; i++)  {
+    let slump = Math.floor(Math.random() * 3)
+    if (slump===0) {
+        console.log('sten')
+    }
+    else if (slump===1) {
+        console.log('sax')
+    }
+    else if (slump===2) {
+        console.log('påse')
+    }
+}
+
+// Lägg till promt för att mata in sten, sax eller påse
+
+//Math.floor(Math.random() * 3)
+
